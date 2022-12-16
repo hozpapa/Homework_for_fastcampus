56c1761887abbb0c910259f09b84ee6299c0dc8f
SongJunghwa <shohoya@gmail.com>
2022-12-16 15:08:00 +0900

    20221216 - 1507 집에서

---
+++ src/namgungjava/Ch09/hashcode/Ex01.java
@@ -0,0 +1,22 @@
+package namgungjava.Ch09.hashcode;
+
+/*
+*
+* 객체의 hashcode를 반환하는 메서드
+* Object클래스의 hashcode()는 객체의 주소를 int로 변환해서 반환
+*
+* public class Object{
+* ...
+* public native int hashcode();
+*
+* native 메서드는 OS의 메서드이다(C언어) -- 자바에서는 JNI 라는 것도 있다네^^
+*
+* equals()를 오버라이딩하면 hashcode()도 오버라이딩 해야 한다.
+* */
+
+
+public class Ex01 {
+    public static void main(String[] args) {
+
+    }
+}
